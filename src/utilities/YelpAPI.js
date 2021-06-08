19b21c96bec8ffb6dc2baafa11680d2e8d054c51
Chounic <hakim.chouioukh@gmail.com>
2021-06-08 23:47:04 +0200

    clean

--- src/utilities/YelpAPI.js
+++ src/utilities/YelpAPI.js
@@ -15,12 +15,8 @@ const YelpAPI = {
               location: location
             }
           })
-          .then( res =>  
-            /*res = JSON.parse(res.data.results.body);
-            const results = this.geojsonify(res.businesses);
-            this.yelpData = results;*/
-            res.data
-          ).then( data => data.filter( business => listevilles.includes(business.location.city)).map( business => {
+          .then( res => res.data)
+          .then( data => data.filter( business => listevilles.includes(business.location.city)).map( business => {
 
             return {
                 id: business.id ,
@@ -40,54 +36,7 @@ const YelpAPI = {
         
     }));
           
-          /*
-        if (businesses){
-
-            businesses.filter( business => listevilles.includes(business.location.city)).map( business => {
-
-                    return {
-                        id: business.id ,
-                        imageSrc: business.image_url ,
-                        name: business.name ,
-                        address: business.location.address1 ,
-                        city: business.location.city ,
-                        state: business.location.state ,
-                        zipCode: business.location.zip_code ,
-                        category: business.categories[0].title ,
-                        rating: business.rating,
-                        reviewCount: business.review_count,
-                        price: business.price,
-                        phone: business.phone,
-                        score: 0
-                    }
-                
-            })
-            
-        }.then( response => response.json()).then(jsonResponse => {
-            console.log(jsonResponse.total);
-            if (jsonResponse.businesses){
-
-                return jsonResponse.businesses.filter( business => listevilles.includes(business.location.city)).map( business => {
-
-                        return {
-                            id: business.id ,
-                            imageSrc: business.image_url ,
-                            name: business.name ,
-                            address: business.location.address1 ,
-                            city: business.location.city ,
-                            state: business.location.state ,
-                            zipCode: business.location.zip_code ,
-                            category: business.categories[0].title ,
-                            rating: business.rating,
-                            reviewCount: business.review_count,
-                            price: business.price,
-                            phone: business.phone,
-                            score: 0
-                        }
-                    
-                })
-            }
-        });*/
+          
     }
 
 };
